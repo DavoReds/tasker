75ab9db2424f275d63ff0f74183c2c98a65b2339
Dalia Rojas Vallejo <daliarojasvallejo@proton.me>
2023-10-30 13:53:07 -0500

    feat(tasks): Implement builder pattern

--- tasker-lib/src/tasks/mod.rs
+++ tasker-lib/src/tasks/mod.rs
@@ -1,33 +1,63 @@
+use derive_builder::Builder;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Builder)]
+#[builder(setter(into))]
 pub struct Task {
     pub description: String,
+
+    #[builder(default)]
     pub state: State,
-    pub tags: Option<Vec<String>>,
+
+    #[builder(default = "Vec::new()")]
+    pub tags: Vec<String>,
+
+    #[builder(default = "self.default_project()")]
+    pub project: String,
 }
 
-#[derive(Debug, Deserialize, Serialize, Default)]
+impl Task {
+    pub fn create(description: &str) -> TaskBuilder {
+        let mut task_builder = TaskBuilder::create_empty();
+        task_builder.description = Some(description.to_string());
+
+        task_builder
+    }
+}
+
+impl TaskBuilder {
+    fn default_project(&self) -> String {
+        "Inbox".to_string()
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize, Default, PartialEq, Eq, Clone)]
 pub enum State {
     #[default]
     ToDo,
     Doing,
     Done,
+    Waiting,
 }
 
-impl Task {
-    pub fn new(description: String) -> Self {
-        Self {
-            description,
-            state: State::default(),
-            tags: None,
-        }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn task_builder_works() -> Result<(), TaskBuilderError> {
+        let task = Task::create("This is a test").build()?;
+
+        assert_eq!(
+            task,
+            Task {
+                description: "This is a test".to_string(),
+                state: State::ToDo,
+                tags: Vec::new(),
+                project: "Inbox".to_string()
+            }
+        );
+
+        Ok(())
     }
-}
-
-#[derive(Debug)]
-pub struct TaskBuilder {
-    pub description: String,
-    pub state: State,
-    pub tags: Option<Vec<String>>,
 }
