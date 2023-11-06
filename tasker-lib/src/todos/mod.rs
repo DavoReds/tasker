3a2434989b1e4a2213aa623c362b495cfdbedc81
Dalia Rojas Vallejo <daliarojasvallejo@proton.me>
2023-11-06 12:09:41 -0500

    feat(to-do): Move add_task implementation to tasker-lib

--- tasker-lib/src/todos/mod.rs
+++ tasker-lib/src/todos/mod.rs
@@ -3,7 +3,7 @@ pub use tasks::*;
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize, Clone, Default)]
+#[derive(Debug, Deserialize, Serialize, Clone, Default, PartialEq, Eq)]
 pub struct ToDo {
     pub tasks: Vec<Task>,
 }
@@ -11,5 +11,35 @@ pub struct ToDo {
 impl From<Vec<Task>> for ToDo {
     fn from(tasks: Vec<Task>) -> Self {
         Self { tasks }
+    }
+}
+
+impl ToDo {
+    pub fn add_task(&mut self, task: Task) {
+        self.tasks.push(task);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_task_works() {
+        let mut todo = ToDo::from(vec![Task::create("This is a test")
+            .project("Testing")
+            .build()]);
+
+        todo.add_task(Task::create("This is from a method").build());
+
+        assert_eq!(
+            todo,
+            ToDo {
+                tasks: vec![
+                    Task::create("This is a test").project("Testing").build(),
+                    Task::create("This is from a method").build()
+                ]
+            }
+        );
     }
 }
